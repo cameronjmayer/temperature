131ffaf517374ee2cb267846458f34311c57c824
cameronjmayer <cmayer0121@skilledkc.org>
2020-10-26 13:28:30 -0500

    Edit 2
    Added commentary

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ function temperatureConverter(valNum) {
     valNum = parseFloat(valNum);
     document.getElementById("outputCelcius").innerHTML=(valNum-32)/1.8;
   }
+  //insert value, calculate the outputCelcius by subtraciting 32 from input value, and then dividing it by 1.8//
